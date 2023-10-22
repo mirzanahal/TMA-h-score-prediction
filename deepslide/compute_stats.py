6296ee2597fd6ab0a98e76857d97ade88d55ccef
nahal mirzaie <mirzanahal@gmail.com>
2023-10-22 15:45:30 +0330

    patch with deepslide

---
+++ deepslide/compute_stats.py
@@ -0,0 +1,162 @@
+"""
+DeepSlide
+Computes the image statistics for normalization.
+
+Authors: Naofumi Tomita
+"""
+import argparse
+import json
+from datetime import datetime
+from pathlib import Path
+from typing import (List, Tuple)
+from tqdm import tqdm
+
+import torch
+from PIL import Image
+from torchvision.transforms import ToTensor
+
+Image.MAX_IMAGE_PIXELS = None
+
+
+
+class MyDataset(torch.utils.data.Dataset):
+        """
+        Creates a dataset by reading images.
+
+        Attributes:
+            data: List of the string image filenames.
+        """
+        def __init__(self, folder: Path) -> None:
+            """
+            Create the MyDataset object.
+
+            Args:
+                folder: Path to the images.
+            """
+            self.data = []
+
+            for file in folder.rglob(f"*{'jpg'}"):
+                if not file.name.startswith("."):
+                    self.data.append(file)
+
+        def __getitem__(self, index: int) -> torch.Tensor:
+            """
+            Finds the specified image and outputs in correct format.
+
+            Args:
+                index: Index of the desired image.
+
+            Returns:
+                A PyTorch Tensor in the correct color space.
+            """
+
+            return ToTensor()(Image.open(self.data[index]).convert("RGB"))
+
+        def __len__(self) -> int:
+            return len(self.data)
+
+
+def compute_stats(folderpath: Path,
+                  image_ext: str) -> Tuple[List[float], List[float]]:
+    """
+    Compute the mean and standard deviation of the images found in folderpath.
+
+    Args:
+        folderpath: Path containing images.
+        image_ext: Extension of the image files.
+
+    Returns:
+        A tuple containing the mean and standard deviation for the images over the channel, height, and width axes.
+
+    This implementation is based on the discussion from: 
+        https://discuss.pytorch.org/t/about-normalization-using-pre-trained-vgg16-networks/23560/9
+    """
+    
+
+    def online_mean_and_sd(
+        loader: torch.utils.data.DataLoader, report_interval: int=1000
+                           ) -> Tuple[List[float], List[float]]:
+        """
+        Computes the mean and standard deviation online.
+            Var[x] = E[X^2] - (E[X])^2
+
+        Args:
+            loader: The PyTorch DataLoader containing the images to iterate over.
+            report_interval: Report the intermediate results every N items. (N=0 to suppress reporting.)
+
+        Returns:
+            A tuple containing the mean and standard deviation for the images
+            over the channel, height, and width axes.
+        """
+        # inputs, classes = next(iter(loader))   
+        # print(inputs, classes)
+
+        cnt = 0
+        fst_moment = torch.empty(3)
+        snd_moment = torch.empty(3)
+        
+        for i, data in tqdm(enumerate(loader, 1)):
+            b, __, h, w = data.shape
+            nb_pixels = b * h * w
+            fst_moment = (cnt * fst_moment +
+                          torch.sum(data, dim=[0, 2, 3])) / (cnt + nb_pixels)
+            snd_moment = (cnt * snd_moment + torch.sum(
+                data**2, dim=[0, 2, 3])) / (cnt + nb_pixels)
+            cnt += nb_pixels
+            if report_interval != 0 and i % report_interval == 0:
+                temp_mean = fst_moment.tolist()
+                temp_std = torch.sqrt(snd_moment - fst_moment**2).tolist()
+                print(f"Mean: {temp_mean}; STD: {temp_std} at iter: {i}")
+        return fst_moment.tolist(), torch.sqrt(snd_moment -
+                                               fst_moment**2).tolist()
+
+    loader = torch.utils.data.DataLoader(
+        dataset=MyDataset(folder=folderpath),
+        batch_size=1,
+        num_workers=1,
+        shuffle=False)
+    print(len(loader))
+    return online_mean_and_sd(loader=loader)
+
+def save_stats(mean: List, std: List, datapath: str):
+    data = {
+        'mean': mean,
+        'std': std,
+        'datapath': datapath}
+    data = json.dumps(data, indent=4)
+    filename = f"stats_{datetime.now().strftime('%Y-%m-%d_%H:%M')}.json"
+    with open(filename, 'w') as outfile:
+        outfile.write(data)
+
+    print(f"Results are saved in {filename}.")
+
+def load_stats(jsonfile: str):
+    """ Load a stats file in json and return mean and std in lists.
+    """
+    with open(jsonfile, 'r') as infile:        
+        data = json.load(infile)
+
+    print(f"Stats of \'{data['datapath']}\' are loaded from {jsonfile}.")
+    return data['mean'], data['std']
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Compute channel-wise patch color mean and std.')
+    parser.add_argument('--datapath', '-i', type=str, required=True,
+        help='Path containing images.')
+    parser.add_argument('--image_ext', '-x', type=str, default='.png',
+        help='Specify file extension of images. Default: .png')
+    parser.add_argument('--report_interval', '-n', type=int, default=1000,
+        help='Report the intermediate results every N items. Default: 1000')
+    parser.add_argument('--save_results', '-d', action='store_true', default=False,
+        help='Set this flag to save results.')
+    args = parser.parse_args()
+
+    mean, std = compute_stats(Path(args.datapath), args.image_ext,)
+    print(f"Mean: {mean}; STD: {std}")
+
+    if args.save_results:
+        save_stats(mean=mean, std=std, datapath=args.datapath)
+
+
---
+++ deepslide/utils.py
@@ -0,0 +1,172 @@
+"""
+DeepSlide
+General helper methods used in other functions.
+
+Authors: Jason Wei, Behnaz Abdollahi, Saeed Hassanpour
+"""
+
+import datetime
+import numpy as np
+
+from pathlib import Path
+from typing import (Dict, List)
+
+# Valid image extensions.
+IMAGE_EXTS = [".jpg", ".jpeg", ".png", ".svs", ".tif", ".tiff"]
+
+
+def get_classes(folder: Path) -> List[str]:
+    """
+    Find the classes for classification.
+
+    Args:
+        folder: Folder containing the subfolders named by class.
+
+    Returns:
+        A list of strings corresponding to the class names.
+    """
+    return sorted([f.name for f in folder.iterdir() if
+                   ((folder.joinpath(f.name).is_dir()) and (".DS_Store" not in f.name))], key=str)
+
+
+def get_log_csv_name(log_folder: Path) -> Path:
+    """
+    Find the name of the CSV file for logging.
+
+    Args:
+        log_folder: Folder to save logging CSV file in.
+
+    Returns:
+        The path including the filename of the logging CSV file with date information.
+    """
+    now = datetime.datetime.now()
+
+    return log_folder.joinpath(f"log_{now.month}{now.day}{now.year}"
+                               f"_{now.hour}{now.minute}{now.second}.csv")
+
+
+def get_image_names(folder: Path) -> List[Path]:
+    """
+    Find the names and paths of all of the images in a folder.
+
+    Args:
+        folder: Folder containing images (assume folder only contains images).
+
+    Returns:
+        A list of the names with paths of the images in a folder.
+    """
+    return sorted([Path(f.name) for f in folder.iterdir() if
+                   ((folder.joinpath(f.name).is_file()) and (".DS_Store" not in f.name) and (f.suffix.casefold() in IMAGE_EXTS))], key=str)
+
+
+def get_image_paths(folder: Path) -> List[Path]:
+    """
+    Find the full paths of the images in a folder.
+
+    Args:
+        folder: Folder containing images (assume folder only contains images).
+
+    Returns:
+        A list of the full paths to the images in the folder.
+    """
+    return sorted([folder.joinpath(f.name) for f in folder.iterdir() if
+                   ((folder.joinpath(f.name).is_file()) and (".DS_Store" not in f.name) and (f.suffix.casefold() in IMAGE_EXTS))], key=str)
+
+
+def get_subfolder_paths(folder: Path) -> List[Path]:
+    """
+    Find the paths of subfolders.
+
+    Args:
+        folder: Folder to look for subfolders in.
+
+    Returns:
+        A list containing the paths of the subfolders.
+    """
+    return sorted([folder.joinpath(f.name) for f in folder.iterdir() if
+                   ((folder.joinpath(f.name).is_dir()) and (".DS_Store" not in f.name))], key=str)
+
+
+def get_all_image_paths(master_folder: Path) -> List[Path]:
+    """
+    Finds all image paths in subfolders.
+
+    Args:
+        master_folder: Root folder containing subfolders.
+
+    Returns:
+        A list of the paths to the images found in the folder.
+    """
+    all_paths = []
+    subfolders = get_subfolder_paths(folder=master_folder)
+    if len(subfolders) > 1:
+        for subfolder in subfolders:
+            all_paths += get_image_paths(folder=subfolder)
+    else:
+        all_paths = get_image_paths(folder=master_folder)
+    return all_paths
+
+
+def get_csv_paths(folder: Path) -> List[Path]:
+    """
+    Find the CSV files contained in a folder.
+
+    Args:
+        folder: Folder to search for CSV files.
+
+    Returns:
+        A list of the paths to the CSV files in the folder.
+    """
+    return sorted([folder.joinpath(f.name) for f in folder.iterdir() if (
+                (folder.joinpath(f.name).is_file()) and ("csv" in f.name) and (".DS_Store" not in f.name))],
+                  key=str)
+
+
+def create_labels(csv_path: Path) -> Dict[str, str]:
+    """
+    Read the labels from a CSV file.
+
+    Args:
+        csv_path: Path to the CSV file.
+
+    Returns:
+        A dictionary mapping string filenames to string labels.
+    """
+    with csv_path.open(mode="r") as lines_open:
+        lines = lines_open.readlines()[1:]
+
+        file_to_gt_label = {}
+
+        for line in lines:
+            if len(line) > 3:
+                pieces = line[:-1].split(",")
+                file = pieces[0]
+                gt_label = pieces[1]
+                file_to_gt_label[file] = gt_label
+
+    return file_to_gt_label
+
+
+# def rgb2hsv(rgb):
+#     """ convert RGB to HSV color space
+
+#     :param rgb: np.ndarray
+#     :return: np.ndarray
+#     """
+
+#     rgb = rgb.astype('float')
+#     maxv = np.amax(rgb, axis=2)
+#     maxc = np.argmax(rgb, axis=2)
+#     minv = np.amin(rgb, axis=2)
+#     minc = np.argmin(rgb, axis=2)
+
+#     hsv = np.zeros(rgb.shape, dtype='float')
+#     hsv[maxc == minc, 0] = np.zeros(hsv[maxc == minc, 0].shape)
+#     hsv[maxc == 0, 0] = (((rgb[..., 1] - rgb[..., 2]) * 60.0 / (maxv - minv + np.spacing(1))) % 360.0)[maxc == 0]
+#     hsv[maxc == 1, 0] = (((rgb[..., 2] - rgb[..., 0]) * 60.0 / (maxv - minv + np.spacing(1))) + 120.0)[maxc == 1]
+#     hsv[maxc == 2, 0] = (((rgb[..., 0] - rgb[..., 1]) * 60.0 / (maxv - minv + np.spacing(1))) + 240.0)[maxc == 2]
+#     hsv[maxv == 0, 1] = np.zeros(hsv[maxv == 0, 1].shape)
+#     hsv[maxv != 0, 1] = (1 - minv / (maxv + np.spacing(1)))[maxv != 0]
+#     hsv[..., 2] = maxv
+
+#     return hsv
